b9f0106e53ea42dc727d6705bea4ee59fda3a66f
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-11 03:29:16 +0600

    added to post method

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const express =require('express');
-const app=express();
-const cors =require('cors')
-const port =process.env.PORT||5000;
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
 
 app.use(cors())
+app.use(express.json());
 
-const user =[
-    {id:1,name:'Janina',gmail:'janina@gmail.com'},
-    {id:2,name:'Pappo',gmail:'pappo@gmail.com'},
-    {id:3,name:'Alamin',gmail:'alamin@gmail.com'},
-    {id:4,name:'Noman',gmail:'noman@gmail.com'},
+
+const user = [
+    { id: 1, name: 'Janina', email: 'janina@gmail.com' },
+    { id: 2, name: 'Pappo', email: 'pappo@gmail.com' },
+    { id: 3, name: 'Alamin', email: 'alamin@gmail.com' },
+    { id: 4, name: 'Noman', email: 'noman@gmail.com' },
 ]
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
 
-  app.get('/user',(req,res)=>{
+app.get('/user', (req, res) => {
     res.send(user)
-  })
+})
 
-app.listen(port,()=>{
+app.post('/user', (req, res) => {
+    console.log('request hetting');
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = user.length + 1;
+    user.push(newUser);
+    res.send(newUser)
+})
+
+app.listen(port, () => {
     console.log('server is runing', port);
 })
