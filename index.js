e597f6f40fb955e2115f31307b339c9023a7507f
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-11 02:04:44 +0600

    added to cors

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express =require('express');
 const app=express();
+const cors =require('cors')
 const port =process.env.PORT||5000;
+
+app.use(cors())
+
+const user =[
+    {id:1,name:'Janina',gmail:'janina@gmail.com'},
+    {id:2,name:'Pappo',gmail:'pappo@gmail.com'},
+    {id:3,name:'Alamin',gmail:'alamin@gmail.com'},
+    {id:4,name:'Noman',gmail:'noman@gmail.com'},
+]
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+  })
+
+  app.get('/user',(req,res)=>{
+    res.send(user)
   })
 
 app.listen(port,()=>{
